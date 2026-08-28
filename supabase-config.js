12daad742f00c5ce28e4eb66a997c9877770447e
fundaonlineacademy-cell <fundaonlineacademy@gmail.com>
2026-08-28 11:17:16 +0200

    Correct Supabase publishable key after Library activation

--- supabase-config.js
+++ supabase-config.js
@@ -2,7 +2,7 @@ window.SUPABASE_URL =
   "https://nzwfowwoazmpnwfrednh.supabase.co";
 
 window.SUPABASE_ANON_KEY =
-  "sb_publishable_fuB02obKKaki0dRsirVVAwWyo99pVP";
+  "sb_publishable_fuB02obKKaki0dRsirVVAw_Wyo99pVP";
 
 window.FUNDA_ADMIN_EMAIL = "";
 
